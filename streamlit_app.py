511188e600c2efe29b5b5619f7c08d68f7d614a6
manojk08 <manojkpr92806@gmail.com>
2025-11-25 12:28:39 +0530

    changed deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,7 +181,7 @@ with st.sidebar:
     with st.expander("🔧 Advanced Settings"):
         api_url = st.text_input(
             "API Endpoint",
-            value="http://localhost:8000/recommend",
+            value="https://talentlens-gdmn.onrender.com/recommend",
         )
     
     st.markdown("---")
